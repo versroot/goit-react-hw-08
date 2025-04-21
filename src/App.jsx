9706ccbcb7a80081d853f60e3ddcb49755d5d203
versroot <131954932+versroot@users.noreply.github.com>
2025-04-21 19:23:29 +0200

    final fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { refreshUser } from './redux/auth/operations';
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout/Layout';
-import HomePage from './pages/HomePage/HomePage';
-import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import ContactsPage from './pages/ContactsPage/ContactsPage';
-import { PrivateRoute } from './routes/PrivateRoute';
-import { RestrictedRoute } from './routes/RestrictedRoute';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import { refreshUser } from "./redux/auth/operations";
+import Layout from "./components/Layout/Layout";
+import HomePage from "./pages/HomePage/HomePage";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ContactsPage from "./pages/ContactsPage/ContactsPage";
+import { PrivateRoute } from "./routes/PrivateRoute";
+import { RestrictedRoute } from "./routes/RestrictedRoute";
+import { selectIsRefreshing } from "./redux/auth/selectors";
 
 export default function App() {
   const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
+  return isRefreshing ? (
+    <p>Refreshing user...</p>
+  ) : (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
@@ -28,7 +32,6 @@ export default function App() {
         <Route element={<PrivateRoute />}>
           <Route path="contacts" element={<ContactsPage />} />
         </Route>
-        <Route path="*" element={<HomePage />} />
       </Route>
     </Routes>
   );
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,15 @@
-// src/components/ContactList/ContactList.jsx
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { fetchContacts } from "../../redux/contacts/operations";
 import {
   selectContacts,
   selectContactsLoading,
-  selectContactsError,
 } from "../../redux/contacts/selectors";
 import { selectFilter } from "../../redux/filters/selectors";
 import {
   selectIsLoggedIn,
   selectIsRefreshing,
 } from "../../redux/auth/selectors";
-import { logout } from "../../redux/auth/operations";
 import css from "./ContactList.module.css";
 import Contact from "../Contact/Contact";
 
@@ -20,28 +17,16 @@ export default function ContactList() {
   const dispatch = useDispatch();
   const contacts = useSelector(selectContacts) || [];
   const isLoading = useSelector(selectContactsLoading);
-  const error = useSelector(selectContactsError);
   const filter = useSelector(selectFilter).toLowerCase();
 
   const isRefreshing = useSelector(selectIsRefreshing);
   const isLoggedIn = useSelector(selectIsLoggedIn);
-
-  // Ensure logout is only dispatched once
-  const hasLoggedOut = useRef(false);
 
   useEffect(() => {
     if (!isRefreshing && isLoggedIn) {
       dispatch(fetchContacts());
     }
   }, [dispatch, isRefreshing, isLoggedIn]);
-
-  useEffect(() => {
-    const msg = typeof error === "string" ? error : error?.message;
-    if (msg === "Please authenticate" && !hasLoggedOut.current) {
-      hasLoggedOut.current = true; // prevent multiple logout dispatches
-      dispatch(logout());
-    }
-  }, [error, dispatch]);
 
   const visible = contacts.filter((c) => c.name.toLowerCase().includes(filter));
 
